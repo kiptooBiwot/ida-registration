fb44e9d9dff9890a859a0fad1c2f0b3de740a2d6
Elvis <elviskb@gmail.com>
2024-07-07 15:28:29 +0300

    maintenance to production

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,176 @@
-<script setup>
-definePageMeta({
-  layout: 'maintenance',
-})
-</script>
 <template>
-  <div
-    class="w-full h-screen flex items-center bg-gradient-to-r from-[#381E4F] to-[#1B2E5F] min-h-screen"
-  >
-    <div class="max-w2xl mx-auto md:mx-auto space-y-5">
-      <h1 class="text-white text-4xl font-title">
-        This site is under maintenance. Try again later.
-      </h1>
-      <div>
-        <img src="/images/maintenance.svg" alt="" class="w-[400px] mx-auto" />
+  <div key=" z-0  min-h-screen w-full ">
+    <section id="home" class="flex items-center bg-gradient-to-r from-[#381E4F]  to-[#1B2E5F] min-h-screen">
+      <div class="mx-10 mt-[100px] md:mt-0  max-w-6xl xl:mx-auto grid grid-cols-2 h-full">
+        <div class="col-span-12 md:col-span-1 h-full flex items-center">
+          <div class="w-fit ml-0 space-y-10">
+            <!-- <h1 class="font-semibold text-gray-50 text-3xl md:text-5xl">Harnessing the <span class="text-blue-400">Power of
+                Digitization</span>
+              through Regenerative
+              Agriculture and
+              Circular Value-Chains</h1> -->
+            <div class="space-y-5">
+              <h1 class="font-semibold text-gray-50 text-5xl md:text-6xl">
+                Building a <span class="text-blue-400">Thriving</span> Future Where <span class="text-blue-400">Design</span> Meets <span class="text-blue-400">Circularity</span>
+              </h1>
+              <h3 class="font-medium text-lg md:text-2xl text-gray-100">Reimagine. Redesign. Regenerate.</h3>
+            </div>
+            <div class="">
+              <NuxtLink to="/registration"
+                class="py-3 px-6 text-sm md:py-4 md:px-8 rounded-md bg-[#673F7B] text-white uppercase">
+                Register
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+        <div class="col-span-12 md:col-span-1 flex items-center justify-center md:justify-end">
+          <!-- <div class="flex items-center justify-center"> -->
+          <!-- <img src="/images/hero_logo_1.svg" alt="" class="w-[300px] h-[300px] md:w-[450px] md:h-[400px]"> -->
+          <!-- </div> -->
+          <img src="/images/hero-img.png" alt="" class="object-fit w-[300px] h-[300px] md:w-[450px] md:h-[450px]">
+        </div>
       </div>
-    </div>
+    </section>
+
+    <!-- About US -->
+    <section id="about-us" class="relative w-full min-h-screen bg-white py-10 md:py-20">
+      <div class="max-w-3xl mx-5 md:mx-auto space-y-10 ">
+        <h2 class="font-title text-center text-3xl md:text-4xl">About Us</h2>
+
+        <p>
+          IDA is a pioneering circular economic tech firm fueled by a powerful vision: a world where design thinking and circularity join forces to create a thriving future.
+        </p>
+
+        <blockquote class="border-l-4 border-purple-800 pl-4 py-2 my-4">
+          <h4 class="italic text-2xl font-semibold pl-4 py-2 my-4 text-gray-700">
+            We believe that by fundamentally re-imagining how we design and build our world, we can redesign systems that regenerate the planet.
+          </h4>
+        </blockquote>
+        <p>
+          Our mission is to unlock the transformative potential of circularity in the built environment, food systems, and energy landscapes on a global scale.
+        </p>
+
+        <!-- <p>
+          Welcome to the Invictus Development Africa (IDA), the tech platform that's changing the game for regenerative agriculture and the circular economy. We're on a mission to empower small farmers by giving them the tools and resources they need to thrive in an ever-changing world. 
+        </p> -->
+        <!-- <blockquote class="border-l-4 border-purple-800 pl-4 py-2 my-4"> -->
+  <!-- <p class="text-lg font-medium">"The best way to predict the future is to invent it."</p> -->
+  <!-- <h4 class="italic text-lg font-semibold pl-4 py-2 my-4 text-gray-700">
+We are the Connective thread. Re-engineering Africa's Agricultural Value Chain to ensure efficacy and prospective products for various markets.
+        </h4> -->
+  <!-- <cite class="block text-right">— IDA</cite> -->
+<!-- </blockquote> -->
+        <!-- <p>
+          We believe that farmers are the heart of our business. That's why we've created a farmer-centric model that puts them at the centre of our operations. We work with smallholder farmers to help them be more productive, profitable, and marketable by providing them with the inputs, training, and market access they need to succeed.
+        </p> -->
+      </div>
+    </section>
+
+
+    <!-- Our Work Section -->
+    <section class="min-h-screen relative w-full bg-gradient-to-r from-[#381E4F]  to-[#1B2E5F] py-10 md:py-20">
+      <div class="max-w-4xl mx-5 lg:mx-auto">
+        <h3 class="text-center text-3xl text-gray-50 mb-5">Our Work</h3>
+        <div class=" grid grid-cols-12 lg:gap-10 text-white">
+        
+          <div class="hidden lg:col-span-4 lg:flex items-center">
+            <img src="/images/abst-2.png" alt="" class="object-cover animate-pulse h-[400px]">
+          </div>
+          <div class="col-span-12 lg:col-span-8 grid gap-5 text-center">
+            <div class="space-y-2">
+              <h4 class="text-blue-400 text-lg text-center">Built Environment</h4>
+              <p class="leading-relaxed">
+                We empower communities, local governments, and development companies to design and build structures that are not just efficient but also regenerative. We champion using recycled materials, biomimicry in design, and optimised building lifecycles to minimise waste and foster a healthier planet.
+              </p>
+            </div>
+            <div>
+              <h4 class="text-blue-400 text-lg text-center">Food Systems</h4>
+            <p class="leading-relaxed">
+              We champion closed-loop food production models that minimise waste and maximise resource utilisation. We support initiatives like urban agriculture, regenerative agriculture practices, and aquaponics systems that promote local food security, resource conservation, and a more resilient food chain.
+            </p>
+            </div>
+            <div>
+              <h4 class="text-blue-400 text-lg text-center">Energy Systems</h4>
+            <p class="leading-relaxed">
+              We play a crucial role in developing renewable energy solutions and promoting energy efficiency through a circular lens. This includes exploring innovative energy storage solutions, promoting circular material lifecycles for renewable energy infrastructure, and advocating for smart grid technologies to create a more resilient and sustainable energy future.
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- Next section -->
+    <section class="min-h-screen py-20">
+      <div class="max-w-5xl mx-5 lg:mx-auto grid grid-cols-12 gap-0 lg:gap-10">
+        <div class="col-span-12 lg:col-span-8 space-y-5 text-justify leading-relaxed">
+
+          <h3 class="text-2xl md:text-3xl text-center">Agriculture Value-Chain Network (AVN)</h3>
+
+         <p>
+          AVN, the flagship prototype from the <a href="#home" class="text-purple-800 font-semibold hover:underline"> Invictus Development Africa (IDA),</a> is revolutionising agriculture. It's not just a hybrid farming system; it's a data-driven powerhouse that empowers farmers to make informed decisions and maximise profits.
+          </p>
+
+<p>
+  <span class="font-semibold">Imagine this: </span> growing exactly what the market demands, optimising resource use, and ensuring a transparent, ethical, and sustainable food chain. AVN makes it possible!
+</p>
+
+<p class="font-bold text-lg">
+Here's how AVN works:
+</p>
+<ul class="list-inside list-decimal space-y-4 ml-0 md:ml-10">
+  <li>
+    <span class="font-semibold text-purple-700">Data-Driven Decisions:</span> AVN leverages environmental, market, and resource data to guide planting choices. Grow what thrives in your climate and sells best, minimising waste and maximising yield.
+  </li>
+  <li>
+    <span class="font-semibold text-purple-700">Hybrid Farming Power:</span> AVN integrates traditional knowledge with modern technology, promoting sustainable practices for long-term success.
+  </li>
+  <li>
+    <span class="font-semibold text-purple-700">Empowering Farmers:</span> AVN connects you directly with distributors, retailers, and suppliers, fostering a more transparent and efficient value chain for organic food.</li>
+</ul>
+    
+    
+
+
+<p class="font-bold text-lg">
+Join the AVN revolution and:
+</p>
+
+<ul class="ml-0 md:ml-10">
+  <li>
+    <span class="font-semibold text-purple-700">Increase profits:</span> grow what the market demands and reduce waste.
+  </li>
+  <li>
+    <span class="font-semibold text-purple-700">Enhance sustainability:</span> optimise resource use and promote environmentally friendly practices.
+  </li>
+  <li>
+    <span class="font-semibold text-purple-700">Gain market access:</span> Connect directly with buyers and get the best prices for your crops.
+  </li>
+</ul>
+
+    
+    
+    
+
+
+<p class="text-center text-purple-800 italic">AVN is the future of agriculture. Be a part of it.</p>
+<div class="flex w-full">
+  <NuxtLink to="/registration" class="mx-auto bg-purple-800 text-white py-2 px-6 shadow-md rounded-md hover:bg-purple-700">Register</NuxtLink>
+</div>
+        </div>
+        <div class="hidden lg:col-span-4 lg:flex items-center justify-center">
+          <div class="flex flex-col items-center">
+            <img src="/images/avn_logo.png" alt="" class="object-contain w-full h-full">
+            <h5 class="text-[#7B4988] text-4xl">A.V.N</h5>
+          </div>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
+
+<script setup>
+
+</script>
---
+++ pages/index_maintenance.vue
@@ -0,0 +1,19 @@
+<script setup>
+definePageMeta({
+  layout: 'maintenance',
+})
+</script>
+<template>
+  <div
+    class="w-full h-screen flex items-center bg-gradient-to-r from-[#381E4F] to-[#1B2E5F] min-h-screen"
+  >
+    <div class="max-w2xl mx-auto md:mx-auto space-y-5">
+      <h1 class="text-white text-4xl font-title">
+        This site is under maintenance. Try again later.
+      </h1>
+      <div>
+        <img src="/images/maintenance.svg" alt="" class="w-[400px] mx-auto" />
+      </div>
+    </div>
+  </div>
+</template>
